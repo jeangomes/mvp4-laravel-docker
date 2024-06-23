5d8f3555ed0c8478373d2e227989cd08b8fb8370
Jean Gomes <jeangomes.ti@gmail.com>
2024-06-23 17:47:03 -0300

    initialize app

---
+++ app/Http/Controllers/FinancialAssetController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreFinancialAssetRequest;
+use App\Http\Requests\UpdateFinancialAssetRequest;
+use App\Models\FinancialAsset;
+
+class FinancialAssetController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreFinancialAssetRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(FinancialAsset $financialAsset)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(FinancialAsset $financialAsset)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateFinancialAssetRequest $request, FinancialAsset $financialAsset)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(FinancialAsset $financialAsset)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_06_23_203315_create_financial_assets_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial_assets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code', 20);
+            $table->boolean('is_foreigner')->default(false);
+            $table->enum('asset_type', ['ETF', 'Stock', 'FII', 'Crypto', 'RF']);
+            $table->enum('stock_type', ['ON', 'PN', 'UNIT']);
+            $table->string('cnpj', 20)->nullable();
+            $table->string('fii_admin_name', 20)->nullable();
+            $table->string('fii_admin_cnpj', 20)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial_assets');
+    }
+};
