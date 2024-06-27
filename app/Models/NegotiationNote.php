09ea809a0579f037522165a487e05473ed619dbf
Jean Gomes <jeangomes.ti@gmail.com>
2024-06-27 20:33:51 -0300

    feat: set casts

--- app/Models/NegotiationNote.php
+++ app/Models/NegotiationNote.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class NegotiationNote extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+             'data_pregao' => 'date',
+            'valor_liquido' => 'float',
+            'taxa_liquidacao' => 'float',
+            'emolumentos' => 'float',
+            'total_taxa' => 'float',
+            'corretagem' => 'float',
+            'liquido' => 'float',
+            'total' => 'float',
+        ];
+    }
 }
