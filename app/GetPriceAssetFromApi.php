3ba6d1ba1104f376a9441b3e53c93e3e7349066e
Jean Gomes <jeangomes.ti@gmail.com>
2024-07-10 23:43:48 -0300

    feat: cache price via api

--- app/GetPriceAssetFromApi.php
+++ app/GetPriceAssetFromApi.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class GetPriceAssetFromApi
@@ -13,9 +14,12 @@ class GetPriceAssetFromApi
      */
     public function __construct($code)
     {
-        $token = config('app.brapi_api_token');
-        $response = Http::get('https://brapi.dev/api/quote/'.$code.'?token='.$token);
-        $result = json_decode($response->body(), true);
-        $this->price = $result['results'][0]['regularMarketPrice'] ?? 0;
+        $seconds = 3600 * 24;
+        $this->price = Cache::remember('assetPrice'.$code, $seconds, function () use ($code) {
+            $token = config('app.brapi_api_token');
+            $response = Http::get('https://brapi.dev/api/quote/'.$code.'?token='.$token);
+            $result = json_decode($response->body(), true);
+            return $result['results'][0]['regularMarketPrice'] ?? 0;
+        });
     }
 }
